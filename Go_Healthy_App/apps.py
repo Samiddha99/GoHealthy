deff0320943d4839113c3b352859e1ded04143b9
Samiddha99 <samiddha99@gmail.com>
2021-01-12 22:17:00 +0530

    Update apps.py

--- Go_Healthy_App/apps.py
+++ Go_Healthy_App/apps.py
@@ -3,7 +3,8 @@ from django.apps import AppConfig
 
 class GoHealthyAppConfig(AppConfig):
     name = 'Go_Healthy_App'
-
+    '''
     def ready(self):
         from Go_Healthy_App.scheduletasks import runtasks
-        #runtasks.start()
+        runtasks.start()
+    '''
