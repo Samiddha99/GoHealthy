6f11fa90da77803338f555ac8e06c5610b18d6ad
Samiddha99 <samiddha99@gmail.com>
2021-01-12 22:17:10 +0530

    Update runtasks.py

--- Go_Healthy_App/scheduletasks/runtasks.py
+++ Go_Healthy_App/scheduletasks/runtasks.py
@@ -1,5 +1,5 @@
 
-#from apscheduler.schedulers.background import BackgroundScheduler
+from apscheduler.schedulers.background import BackgroundScheduler
 import time
 from timeloop import Timeloop
 from datetime import timedelta
@@ -10,20 +10,38 @@ from Go_Healthy_App.scheduletasks import tasks
 
 
 def start():
-    tasks.deleteOTP()
+    scheduler1 = BackgroundScheduler()
+    scheduler1.add_job(tasks.deleteOTP, 'interval', seconds=1)
+    scheduler1.start()
 
-    tasks.deleteResetLink()
+    scheduler2 = BackgroundScheduler()
+    scheduler2.add_job(tasks.deleteResetLink, 'interval', seconds=1)
+    scheduler2.start()
 
-    tasks.bookExpireAlert()
+    scheduler3 = BackgroundScheduler()
+    scheduler3.add_job(tasks.bookExpireAlert, 'interval', seconds=1)
+    scheduler3.start()
 
-    tasks.expireBooking()
+    scheduler4 = BackgroundScheduler()
+    scheduler4.add_job(tasks.expireBooking, 'interval', minutes=10)
+    scheduler4.start()
 
-    tasks.multipleBookingWarning()
+    scheduler5 = BackgroundScheduler()
+    scheduler5.add_job(tasks.multipleBookingWarning, 'interval', minutes=30)
+    scheduler5.start()
 
-    tasks.deleteExpireBooking()
+    scheduler6 = BackgroundScheduler()
+    scheduler6.add_job(tasks.deleteExpireBooking, 'interval', days=1)
+    scheduler6.start()
 
-    tasks.deleteRealese()
+    scheduler7 = BackgroundScheduler()
+    scheduler7.add_job(tasks.deleteRealese, 'interval', days=100)
+    scheduler7.start()
 
-    tasks.deleteNonVerifyUser()
+    scheduler8 = BackgroundScheduler()
+    scheduler8.add_job(tasks.deleteNonVerifyUser, 'interval', days=50)
+    scheduler8.start()
 
-    tasks.changeBedNo()
+    scheduler9 = BackgroundScheduler()
+    scheduler9.add_job(tasks.changeBedNo, 'interval', minutes=1)
+    scheduler9.start()
