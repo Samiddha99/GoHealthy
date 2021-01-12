9b0a8992079d6ddb7007893d71b905d486ae2ab1
Samiddha99 <samiddha99@gmail.com>
2021-01-12 23:41:33 +0530

    Update settings.py

--- GoHealthy/settings.py
+++ GoHealthy/settings.py
@@ -84,8 +84,12 @@ WSGI_APPLICATION = 'GoHealthy.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': env("DATABASE_ENGINE"),
+        'NAME': env("DATABASE_NAME"),
+        'USER': env("DATABASE_USER"),
+        'PASSWORD': env("DATABASE_PASSWORD"),
+        'HOST': env("DATABASE_HOST"),
+        'PORT': env("DATABASE_PORT"),
     }
 }
 
