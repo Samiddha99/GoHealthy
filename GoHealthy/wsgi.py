0ec5928d685d6511ebd060ef14dd1c0a5d04211e
Samiddha99 <samiddha99@gmail.com>
2021-01-13 15:18:33 +0530

    Update wsgi.py

--- GoHealthy/wsgi.py
+++ GoHealthy/wsgi.py
@@ -14,3 +14,4 @@ from whitenoise import WhiteNoise
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'GoHealthy.settings')
 
 application = get_wsgi_application()
+application = WhiteNoise(application)
