887ec32cb0df03184c7add0877b7a6fd8a0c6802
Samiddha99 <samiddha99@gmail.com>
2021-01-13 10:38:44 +0530

    Update settings.py

--- GoHealthy/settings.py
+++ GoHealthy/settings.py
@@ -26,12 +26,12 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # Quick-start development settings - unsuitable for production
 # See https://docs.djangoproject.com/en/3.1/howto/deployment/checklist/
 
-# SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = env.str("SECRET_KEY")
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
 
-ALLOWED_HOSTS = []
+SECRET_KEY = env.str("SECRET_KEY")
+
+DEBUG = False
+
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
