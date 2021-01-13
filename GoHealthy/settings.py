fbf9cd96f95f328c418e71757aeaa6548e22c2df
Samiddha99 <samiddha99@gmail.com>
2021-01-13 14:57:41 +0530

    Update settings.py

--- GoHealthy/settings.py
+++ GoHealthy/settings.py
@@ -87,7 +87,7 @@ MIDDLEWARE = [
 ]
 
 
-#STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
+STATICFILES_STORAGE = 'whitenoise.storage.CompressedManifestStaticFilesStorage'
 
 ROOT_URLCONF = 'GoHealthy.urls'
 
