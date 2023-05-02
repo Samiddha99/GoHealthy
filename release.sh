aa0cd455e0595eff44df5a286f529aefa3fa5702
go-healthy <gohealthymail@gmail.com>
2023-05-02 23:28:43 +0530

    Changes

--- release.sh
+++ release.sh
@@ -2,10 +2,11 @@
 
 echo "Starting the web server................."
 echo "Running release comands"
+python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
 python manage.py crontab add
 python manage.py crontab show
 crontab -l
 # python manage.py runserver 0.0.0.0:$PORT
-daphne -b 0.0.0.0 GoHealthy.asgi:application
+daphne -b 0.0.0.0 -p $PORT GoHealthy.asgi:application
