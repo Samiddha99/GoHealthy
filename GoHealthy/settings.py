340c78965227b2b572d27a9ff4193d50ca43c4b2
go-healthy <gohealthymail@gmail.com>
2023-05-05 13:39:58 +0530

    Changes

--- GoHealthy/settings.py
+++ GoHealthy/settings.py
@@ -205,7 +205,7 @@ if DEPLOY:
     SECURE_HSTS_PRELOAD = True
     SECURE_HSTS_SECONDS = 1 * 365 * 24 * 60 * 60  # 365 days in second
     SECURE_REFERRER_POLICY = 'same-origin'
-    SECURE_SSL_REDIRECT = True  # if any http request come, then convert it to https if possible.
+    # SECURE_SSL_REDIRECT = True  # if any http request come, then convert it to https if possible.
     
     APPEND_SLASH = True # append a slash after the url
     
--- release.sh
+++ release.sh
@@ -5,8 +5,8 @@ echo "Running release comands"
 python manage.py makemigrations
 python manage.py migrate
 python manage.py collectstatic --noinput
-# python manage.py crontab add
-# python manage.py crontab show
-# crontab -l
+python manage.py crontab add
+python manage.py crontab show
+crontab -l
 # python manage.py runserver 0.0.0.0:$PORT
 daphne GoHealthy.asgi:application -b 0.0.0.0 -p $PORT
