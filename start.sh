c89df77d95b25bf12e34f1af4a8c30c75d0570e5
go-healthy <gohealthymail@gmail.com>
2023-07-07 13:16:10 +0530

    change

--- start.sh
+++ start.sh
@@ -8,4 +8,5 @@ echo "Starting the web server................."
 # python manage.py crontab add
 # python manage.py add_all_data
 # python manage.py runserver 0.0.0.0:$PORT
-daphne GoHealthy.asgi:application -b 0.0.0.0 -p $PORT --access-log - --websocket_timeout -1 --websocket_connect_timeout 20 --http-timeout 20
+gunicorn GoHealthy.wsgi --bind 0.0.0.0:$PORT --workers 2 --worker-class gthread --threads 3 --worker-connections 1000 --timeout 30 --keep-alive 5 --access-logfile - --log-file - --log-level info --capture-output
+# daphne GoHealthy.asgi:application -b 0.0.0.0 -p $PORT --access-log - --websocket_timeout -1 --websocket_connect_timeout 20 --http-timeout 20
